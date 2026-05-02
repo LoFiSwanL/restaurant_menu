d0d156a0f8bc4ce8e4bc42a1cd12b82d80881a81
LoFiSwanL <daiki.ren710@gmai.com>
2026-05-02 22:12:11 +0300

    feat: init web api and setup database

---
+++ restaurant_menu/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace NamRestaurantApi.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public List<MenuItem> MenuItems { get; set; } = new();
+    }
+}
